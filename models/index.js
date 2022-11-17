f41ec094e2a8fad75bd25e68615a6efd9baa7258
CoderGirl <angi.adema@gmail.com>
2022-11-16 17:22:25 -0700

    added code to the models index.js file

---
+++ models/index.js
@@ -0,0 +1,31 @@
+//Set up require for all model files.
+const Comments = require('./Comments');
+const Posts = require('./Posts');
+const Users = require('./Users');
+
+//Define table or data relationships.
+Comments.belongsTo(Users, {
+    foreignKey: 'users_id'
+});
+
+Comments.belongsTo(Posts, {
+    foreignKey: 'posts_id'
+});
+
+Posts.belongsTo(Users, {
+    foreignKey: 'users_id'
+});
+
+Posts.hasMany(Comments, {
+    foreignKey: 'posts_id'
+});
+
+Users.hasMany(Comments, {
+    foreignKey: 'users_id'
+});
+
+Users.hasMany(Posts, {
+    foreignKey: 'users_id'
+});
+//Export the file.
+module.exports = { Comments, Posts, Users };
