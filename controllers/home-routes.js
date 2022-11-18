0e432d7e4446de8ce0ed7c4a350c70c34313dae7
CoderGirl <angi.adema@gmail.com>
2022-11-18 16:08:48 -0700

    added code to home-routes.js file

---
+++ controllers/home-routes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const { Comments, Posts, Users } = require('../models');
+
+//GET all posts for the homepage including associated Users data.
+router.get('/', async (req, res) => {
+    try {
+        const dbPostsData = await Posts.findAll({
+            include: [{ model: Comments, through: Users }],
+        });
+        res.status(200).json(dbPostsData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//GET a single post.
+router.get('/:id', async (req, res) => {
+    try {
+        const singlePostData = await Posts.findByPk(req.params.id, {
+            include: [{ model: Comments, through: Users }],
+            
+            const post = singlePostData.get({ plain: true});
+        });
+
+        if (!singlePostData) {
+            res.status(404).json({ message: 'No post found with that id!' });
+            return;
+        }
+        res.status(200).json(singlePostData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
