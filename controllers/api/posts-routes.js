6cef85ce33016ea9c5cfbbcb7e8266a6b82e0e68
CoderGirl <angi.adema@gmail.com>
2022-11-19 20:38:47 -0700

    updated posts-routes.js

---
+++ controllers/api/posts-routes.js
@@ -0,0 +1,62 @@
+const router = require('express').Router();
+const { Posts } = require('../../models');
+const withAuth = require('../../utils');
+
+//Create the post.
+router.post('/', withAuth, async (req, res) => {
+    const body = req.body;
+
+    try {
+        const addedPost = await Posts.create({
+            where: {
+                userId: req.session.userId,
+            },
+        });
+        res.json(addedPost);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//Edit the post.
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+        const updatePost = await Posts.update(
+            req.body,
+            {
+                where: {
+                    id: req.params.id,
+                },
+            });
+
+        if (!updatePost) {
+            res.status(404).json({ message: 'No post found with that id!' });
+            return;
+        }
+        res.status(200).json(updatePost);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//Delete a post.
+router.delete('/:id', withAuth, async (req, res) => {
+    // delete a category by its `id` value
+    try {
+        const deletePost = await Posts.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+
+        if (!deletePost) {
+            res.status(404).json({ message: 'No post found with this id!' });
+        }
+        res.status(200).json(deletePost);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//Export the routes.
+module.exports = router;
