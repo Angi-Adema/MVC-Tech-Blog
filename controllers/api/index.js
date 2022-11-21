4cb1242221a39a77dae1a45fb950e18459420c4b
CoderGirl <angi.adema@gmail.com>
2022-11-20 17:19:11 -0700

    updated api index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,15 +2,15 @@
 const router = require('express').Router();
 
 //Require Comments, Posts and User routes.
-const commentsRoutes = require('./comment-routes');
-const postsRoutes = require('./posts-routes');
-const usersRoutes = require('./users-routes');
+const commentRoutes = require('./comment-routes');
+const postRoutes = require('./post-routes');
+const userRoutes = require('./user-routes');
 
 
 //Use created routes.
-router.use('/comment', commentsRoutes);
-router.use('/post', postsRoutes);
-router.use('/user', usersRoutes);
+router.use('/comment', commentRoutes);
+router.use('/post', postRoutes);
+router.use('/user', userRoutes);
 
 //Export the router.
 module.exports = router;
